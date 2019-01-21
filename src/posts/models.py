ea0e01dcb7ad5bd8d0510cc47f6d3d7930e0a7be
KonradMarzec1991 <konrimarzec@gmail.com>
2019-01-21 01:39:22 +0100

    Models

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+    timestamp = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
